af339c46ebe3ffff8deb4871401bb4dc6fe7d064
auziqni <auziqni@gmail.com>
2023-11-02 13:39:18 +0700

    feat: monitoring udara

--- app/(dashboard)/monitorudara/page.tsx
+++ app/(dashboard)/monitorudara/page.tsx
@@ -1,5 +1,18 @@
 import React from "react";
+import Link from "next/link";
+import { GetDataMonitoringofTen } from "@/lib/getData";
+export const dynamic = "force-dynamic";
+import ChartUdara from "@/components/chartUdara";
 
-export default function page() {
-  return <div className="pt-20">page</div>;
+export default async function DashboardAir() {
+  const DataMonitoring = await GetDataMonitoringofTen();
+
+  return (
+    <main className="pt-20 px-5 h-screen overflow-clip">
+      <h3>
+        Monitoring Udara<Link href={"monitorudara/alldata"}>➡️</Link>
+      </h3>
+      <ChartUdara props={DataMonitoring}></ChartUdara>
+    </main>
+  );
 }
